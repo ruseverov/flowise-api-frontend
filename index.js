c17f22e09b9d84e63b9ae3e6b326d43db76d47dd
Ruseverov <ruseverov@gmail.com>
2023-11-05 00:16:48 -0700

    EgorTest

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotevn.config();
 import { createPrediction } from "./controllers/flowise.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 
